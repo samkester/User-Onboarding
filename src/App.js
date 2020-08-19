e53ab3eecf7d95078d78289d14e3f17993447dc4
Sam Kester <samkester@gmail.com>
2020-08-19 14:06:15 -0600

    stateful form fields

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Form from "./components/Form"
 
+const defaultFormValues = {
+  name: "",
+  email: "",
+  password: "",
+  readTOS: false,
+}
+
 function App() {
+  const [formValues, setFormValues] = useState(defaultFormValues);
+
+  const setFormValue = (name, value) => {
+    setFormValues({...formValues, [name]: value});
+  };
+
   return (
     <div>
-      <Form />
+      <Form values={formValues} setValue={setFormValue} />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,23 +1,31 @@
 import React from "react";
 
-const Form = (props) => {
+const Form = ({values, setValue}) => {
+    const setValueDirect = (event => {
+        setValue(event.target.name, event.target.value)
+    })
+
+    const setValueOfCheckbox = (event => {
+        setValue(event.target.name, event.target.checked)
+    })
+    
     return(
         <div>
             <label>
                 Name
-                <input type="text" name="name" />
+                <input type="text" name="name" value={values.name} onChange={setValueDirect} />
             </label>
             <label>
                 Email
-                <input type="text" name="email" />
+                <input type="text" name="email" value={values.email} onChange={setValueDirect} />
             </label>
             <label>
                 Password
-                <input type="text" name="password" />
+                <input type="text" name="password" value={"*".repeat(values.password.length)} onChange={setValueDirect} />
             </label>
             <label>
                 I agree to the Terms of Service.
-                <input type="checkbox" name="name" />
+                <input type="checkbox" name="readTOS" checked={values.readTOS} onChange={setValueOfCheckbox} />
             </label>
         </div>
     );
